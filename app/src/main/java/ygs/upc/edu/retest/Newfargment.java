3fc279476d122b18ec43de014c4c5ce54b39e21d
machaowudi <1823447269@qq.com>
2020-06-01 15:48:56 +0800

    project

---
+++ app/src/main/java/ygs/upc/edu/retest/Newfargment.java
@@ -0,0 +1,18 @@
+package ygs.upc.edu.retest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Newfargment  extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmentt, container, false);
+        return v;
+    }
+
+}
+
